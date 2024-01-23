a40e67cfbf3e8c8c5cb680b563203288ad2eb686
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-01-23 16:51:37 +0200

    changed to 3002

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'Emerald Medical Aid',
-        port: '3001',
+        port: '3002',
         exec_mode: 'cluster',
         instances: '1',
         script: './.output/server/index.mjs'

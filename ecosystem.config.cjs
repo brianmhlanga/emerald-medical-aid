528cbf1f35c21f893ee33669a8121ee0e4982f1b
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-02-06 16:14:29 +0200

    commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: 'Emerald Medical Aid',
-        port: '3009',
+        port: '3050',
         exec_mode: 'cluster',
         instances: 'max',
         script: './.output/server/index.mjs'

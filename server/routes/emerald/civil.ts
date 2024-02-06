b758b237711b456e372894a278b7dfa6ac1c3eb3
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-02-06 08:20:47 +0200

    Updated civil route

--- server/routes/emerald/civil.ts
+++ server/routes/emerald/civil.ts
@@ -1,5 +1,5 @@
 import { prisma } from "~~/prisma/db";
-
+import { sendCivilEmail } from "~/helpers/emails";
 export default defineEventHandler(async (event)=>{
     
     const { data: {first_name,last_name,title,date_of_birth,id_number,gender,membership_number,cell_number,physical_address,place_of_birth,marital_status,employer_name,employer_contact_number,occupation,department,ec_number,station_number,depandancy,package_type,package_details,previous_ailments,other_information,previous_society_name,previous_package_name,from_date,to_date,ministry,department_code,station_code,selected_application_type,card_type,section,subsection,employee_code_number,cd,payee_code,amount_deducted,from_date_ty,to_date_ty,policy_medical_aid_number}} = await readBody(event);
@@ -67,11 +67,20 @@ export default defineEventHandler(async (event)=>{
      }
      
     })
-
+    const createdProfile = await prisma.individual.findUnique({
+        where: {
+            id: createProfile.id
+        },
+        include: {
+            depandancy: true,
+            tyform: true
+        }
+    })
     
  
     response['profile'] = createProfile
     response['success'] = true
+    await sendCivilEmail(createdProfile)
  
     } catch(error) {
  
@@ -80,8 +89,5 @@ export default defineEventHandler(async (event)=>{
        response['message'] = error.toString()
  
    }
- 
     return response
- 
-   
  });
